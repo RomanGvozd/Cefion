e7e3d7ba4244a4ebe6be181ba09b5e72e058f8d0
Roman Gvozd <r.gvozdev16@gmail.com>
2022-06-02 17:31:05 +0300

    refoctoring code

--- src/components/News/MarketNews/MarketNews.jsx
+++ src/components/News/MarketNews/MarketNews.jsx
@@ -11,41 +11,44 @@ function MarketNews() {
     const [cards, setCards] = useState(arr)
     
     return (
-        <section className='market-news'>
-            <div className='market-news__header'>
-                <h2 className={theme === 'dark' ? 'header__title-dark' : 'header__title-light'}>
-                    {language === "RU"
-                    ? "Технология"
-                    : "Market News"
-                    }
-                </h2>
-            </div>
-            <div className='market-news__content'>
-                {cards.map((card)=>(
-                    <div className={theme === 'dark' ? 'content__card content__card-dark' : 'content__card content__card-light'}>
-                        <div className='card__image'>
-  
-                        </div>
-                        <div className='card__header'>
-                            {language === "RU"
-                            ? <p className='card__header__title'>{card.titleRU}</p>
-                            : <p className='card__header__title'>{card.titleEN}</p>
-                            }
-                            <p className='card__header__user'>
-                                {card.avtor}
-                            </p>
-                        </div>
+        <>
+            <section className='market-news'>
+                <div className='market-news__header'>
+                    <h2 className={theme === 'dark' ? 'header__title-dark' : 'header__title-light'}>
                         {language === "RU"
-                        ? <p className='card__description'>{card.descriptionRU}</p>
-                        : <p className='card__description'>{card.descriptionEN}</p>
+                        ? "Технология"
+                        : "Market News"
                         }
-                        <div className='card__footer'>
+                    </h2>
+                </div>
+                <div className='market-news__content'>
+                    {cards.map((card)=>(
+                        <div className={theme === 'dark' ? 'content__card content__card-dark' : 'content__card content__card-light'}>
+                            <div className='card__image'>
+    
+                            </div>
+                            <div className='card__header'>
+                                {language === "RU"
+                                ? <p className='card__header__title'>{card.titleRU}</p>
+                                : <p className='card__header__title'>{card.titleEN}</p>
+                                }
+                                <p className='card__header__user'>
+                                    {card.avtor}
+                                </p>
+                            </div>
+                            {language === "RU"
+                            ? <p className='card__description'>{card.descriptionRU}</p>
+                            : <p className='card__description'>{card.descriptionEN}</p>
+                            }
+                            <div className='card__footer'>
 
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </div>         
-        </section>
+                    ))}
+                </div>         
+            </section>
+            <div className={theme === "dark" ? "line-dark" : "line-light"}></div>
+        </>
     );
 }
 
