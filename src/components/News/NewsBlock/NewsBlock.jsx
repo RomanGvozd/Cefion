4174e2354c34c2e439c541a9b757c531e568e14a
Roman Gvozd <r.gvozdev16@gmail.com>
2022-06-03 19:42:45 +0300

    create news

--- src/components/News/NewsBlock/NewsBlock.jsx
+++ src/components/News/NewsBlock/NewsBlock.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {useSelector} from "react-redux";
+import { content } from "./newsBlock.config";
 
 import arr from './newsBlockPosts';
 
@@ -9,25 +10,32 @@ function NewsBlock() {
     const theme = useSelector((store) => store.theme.theme);
     const language = useSelector((store) => store.language.language);
 
-    const [posts, setPosts] = useState(arr)
+    const [posts, setPosts] = useState(arr);
+
+  const { title, text1, text2, text3, button1, button2 } = content[language];
+
     
     return (
         <section className="news-block">
             <div className='news-block__image'>
+                <div className='image__top'>
+                    <h4 className='image__top-title'>
+                        {title}
+                    </h4>
+                </div>
+                <div className='image__bottom'>
+                    <p className='image__bottom-text'>{text1}</p>
+                    <p className='image__bottom-text'>{text2}</p>
+                    <p className='image__bottom-text'>{text3}</p>
+                </div>
             </div>
             <div className="news-block__aside">
                 <div className='aside__block-button'>
                     <button className={theme === 'dark' ? 'aside__button-dark' : 'aside__button-light'}>
-                        {language === "RU"
-                        ? "Выбор редактора"
-                        : "Editor's Choice"
-                        }
+                        {button1}
                     </button>
                     <button className={theme === 'dark' ? 'aside__button-dark' : 'aside__button-light'}>
-                        {language === "RU"
-                        ? "ГОРЯЧИЕ НОВОСТИ"
-                        : "HOTNEWS"
-                        }
+                        {button2}
                     </button>
                 </div>
                 <div className="news-block__posts">
---
+++ src/components/News/NewsBlock/newsBlock.config.js
@@ -0,0 +1,18 @@
+export const content = {
+    EN:{
+        title:"New Alfa Romeo SUV uses NFT and blockchain to track car records",
+        text1: "Latest News",
+        text2: "by Arnold Kirimi",
+        text3: "8 hours ago",
+        button1: "Editor's Choice",
+        button2: "HOTNEWS",
+    },
+    RU:{
+        title:"Новый внедорожник Alfa Romeo использует NFT и блокчейн для отслеживания автомобильных рекордов",
+        text1: "Последние новости",
+        text2: "Арнольд Кирими",
+        text3: "8 часов назад",
+        button1: "Выбор редактора",
+        button2: "ГОРЯЧИЕ НОВОСТИ",
+    }
+}
